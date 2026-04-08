f5f68cf3f47a79493374a58d0b32e52729e79ee2
Chaos Pjeles <chaos@efqr.dev>
2026-04-08 10:38:06 +0100

    add summary and description to SEARCH/feeds path #75 (#89)

--- cyberthreatexchange/server/views.py
+++ cyberthreatexchange/server/views.py
@@ -581,7 +581,23 @@ class FeedObjectsView(viewsets.GenericViewSet):
             This endpoint is particularly useful when you don't know the objects you want, or if the concept you're interested in is covered by a framework.
             """
         ),
-    )
+    ),
+    feeds=extend_schema(
+        summary="Get feeds containing an object",
+        description=textwrap.dedent(
+            """
+            Get a list of all feeds containing a specific object.
+            """
+        ),
+        parameters=[
+            OpenApiParameter(
+                "object_id",
+                type=OpenApiTypes.STR,
+                location=OpenApiParameter.PATH,
+                description="The  STIX ID of the Object.",
+            ),
+        ],
+    ),
 )
 class SearchView(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = serializers.StixObjectsPlaceholderSerializer(many=True)
@@ -642,15 +658,19 @@ class SearchView(mixins.ListModelMixin, viewsets.GenericViewSet):
         qs = models.NewObjectValue.objects.filter(is_dupe=False)
         qs = qs.alias(value=F("values_sort"))
         return qs
-    
+
     def get_serializer_context(self):
         ctx = super().get_serializer_context()
-        ctx.update(objects=ArangoDBHelper('semantic_search', self.request).get_context_for_objects(self.object_ids))
+        ctx.update(
+            objects=ArangoDBHelper(
+                "semantic_search", self.request
+            ).get_context_for_objects(self.object_ids)
+        )
         return ctx
-    
+
     # def get_serializer(self, page, *args, **kwargs):
     #     return super().get_serializer(page,*args, **kwargs)
-    
+
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         page = self.paginate_queryset(queryset)
@@ -658,9 +678,12 @@ class SearchView(mixins.ListModelMixin, viewsets.GenericViewSet):
         self.serializer_class = values_serializers.ValuesAsStixSerializer
         serializer = self.get_serializer(page, many=True)
         return self.get_paginated_response(serializer.data)
-    
 
-    @decorators.action(detail=True, methods=["GET"], serializer_class=serializers.ObjectFeedsDetailSerializer)
+    @decorators.action(
+        detail=True,
+        methods=["GET"],
+        serializer_class=serializers.ObjectFeedsDetailSerializer,
+    )
     def feeds(self, request, object_id):
         obj = get_list_or_404(models.NewObjectValue, stix_id=object_id)[0]
         feed_ids = (
